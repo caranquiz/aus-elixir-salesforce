631c27e335530f60096d650aae5cfcaaf0e5a765
Khalid Fazlullah <khalidfazlullah@MacBook-Pro.local>
2019-10-23 16:46:52 +0530

    Backup of squid pages on UAT

--- elixir/Gruntfile.js
+++ elixir/Gruntfile.js
@@ -45,7 +45,63 @@ module.exports = function(grunt) {
     },
 
     });
-  grunt.registerTask('skuid-pull-specific',
+  grunt.registerTask('skuid-pull-uat',
+    'Pull Skuid pages from any Salesforce org running Skuid.',
+    function() {
+      var pageNames = pageNameList.getPageNames();
+      var self = this,
+        done = this.async();
+      //@todo figure out how to use this to prevent bad things
+      //from happening
+      var options = this.options({
+        'page': pageNames,
+        'dest': 'skuidPages/',
+        'mode': 'single',
+        'redirectUri': 'http://localhost:3000/oauth/_callback',
+     'clientId':'3MVG9aWdXtdHRrI0e4qHQ7X8QIBbA6lfLvf3FAHoSdpOG5QbpQzGEFT_uT3fPAyPqlKWL6vTZSEDkAB3LZ4Qn',
+    'clientSecret':'838FC5257B336DAFE6B055D02954C9E7128CE90B44510DFFEB098895EF454AF4',
+    'username':'provisioningcls@38trialforce.com.uatelixir',
+    'password':'Welcome@12345KrK0dBXQJFuLdCF78fyv8x9wr',
+    'nforceOptions': {
+      'environment':'sandbox'
+   }
+      });
+      
+      helpers.validateRequiredOptions(['username','password','clientId', 'clientSecret'], options);
+      if (_.isArray(options.page)){
+        options.page = options.page.join(',');
+      }
+
+      var org = nforce.createConnection(helpers.getOrgOptions(options));
+
+      org.authenticate(helpers.getOrgCredentials(options))
+        .then(function() {
+          return org.apexRest({
+            uri: apexEndpoint,
+            method: 'GET',
+            urlParams: {
+              page: options.page
+            }
+          });
+        })
+        .then(function(response) {
+          response = JSON.parse(response);
+          if (!response.error) {
+            helpers.writeDefinitionFiles(response, path.join(options.dest));
+            grunt.log.ok('Success! Skuid pages for module(s) ' + options.module + ' written to ' + options.dest);
+            done();
+          } else {
+            grunt.fail.fatal(response.error);
+          }
+
+        })
+        .error(function(error) {
+          grunt.fail.fatal(error);
+        });
+
+    });
+
+    grunt.registerTask('skuid-pull-dev',
     'Pull Skuid pages from any Salesforce org running Skuid.',
     function() {
       var pageNames = pageNameList.getPageNames();
@@ -102,7 +158,7 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.registerTask('default', ["skuid-pull-specific"]);
+    grunt.registerTask('default', ["skuid-pull-uat"]);
     grunt.registerTask('pull-code', ["skuid-pull"]);
     grunt.registerTask('push-code', ["skuid-push:specific"]);
     grunt.loadNpmTasks('skuid-grunt');
